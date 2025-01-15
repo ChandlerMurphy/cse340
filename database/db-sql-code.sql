386db30b9d6ea85a64ac93acc669a5b064b6ef0c
ChandlerMurphy <chandlermurphy02@gmail.com>
2025-01-14 17:01:46 -0700

    completed week two assignment and just saving changes before video creation etc.

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -238,3 +238,13 @@ VALUES   (
     'White',
     5
   );
+
+  -- Query 4 week 2 assignment
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+-- Query 6 week 2 assignment
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
