e0c702bfec20f5af00ff988dcfebcfa4f70eaa88
ChandlerMurphy <chandlermurphy02@gmail.com>
2025-01-23 17:55:49 -0700

    Completed Error handling activity for W3. Now just doing some testing and any additional checks before submission.

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
+// New route to trigger 500 error
+app.get("/trigger-error", (req, res, next) => {
+  const newError = new Error('This is an intential 500 error!')
+  newError.status = 500
+  next(newError)
+})
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
@@ -45,7 +52,13 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404){ 
+    message = err.message
+  } if (err.status == 500) {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
